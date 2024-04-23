c351cf7333dac1e0ed584c6d29ce6e7c089cf08d
VicenteHR96 <vhernandezr96@gmail.com>
2024-04-23 17:20:16 -0400

    UPDATE: Vista Detalle producto

--- front/src/views/Pizza/Pizza.jsx
+++ front/src/views/Pizza/Pizza.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { PizzaContext } from "../../contexts/PizzaContext";
-import { Button } from "react-bootstrap";
+import Button from "@mui/material/Button";
+import { Card, CardActions, CardContent, Typography } from "@mui/material";
 
 const Pizza = () => {
   const { id } = useParams();
@@ -26,10 +27,12 @@ const Pizza = () => {
   return (
     <div className="cont-detalle">
       <div className="section-detalle">
-        <img src={pizzaX.img} alt={pizzaX.name} />
-        <div className="detalle-texto">
-          <h3>{pizzaX.name[0].toUpperCase() + pizzaX.name.slice(1)}</h3>
-          <hr />
+        <h3>{pizzaX.name[0].toUpperCase() + pizzaX.name.slice(1)}</h3>
+        <div className="card-detalle">
+          <div className="img-detalle">
+            <img src={pizzaX.img} alt={pizzaX.name} />
+          </div>
+          {/* <div className="detalle-texto">
           <p style={{ textAlign: "justify" }}>{pizzaX.desc}</p>
 
           <h6>Ingredientes:</h6>
@@ -43,6 +46,42 @@ const Pizza = () => {
           <Button variant="danger" onClick={handleClick}>
             Añadir 🛒
           </Button>
+        </div> */}
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <div className="d-flex justify-content-between">
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {pizzaX.name[0].toUpperCase() + pizzaX.name.slice(1)}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  $ {pizzaX.price}
+                </Typography>
+              </div>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Descripción del producto
+              </Typography>
+              <Typography variant="body2">
+                {pizzaX.desc}
+                <br />
+                <ul style={{ listStyle: "none" }}>
+                  {pizzaX.ingredients.map((i, index) => {
+                    return (
+                      <li key={index}>🍕 {i[0].toUpperCase() + i.slice(1)}</li>
+                    );
+                  })}
+                </ul>
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" onClick={handleClick}>
+                Añadir al carrito 🛒
+              </Button>
+            </CardActions>
+          </Card>
         </div>
       </div>
     </div>
